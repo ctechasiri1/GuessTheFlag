201caf22582cc9ae4beef2807527b5bbbebf28d3
Chiraphat <ctechasiri@berkeley.edu>
2024-09-25 17:02:07 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Chiraphat Techasiri on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
